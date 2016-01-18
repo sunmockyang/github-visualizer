9bfb68c00e3de08fcc6a4c754b700d255d6c4ed5
Sunmock Yang <sunmock@live.ca>
2016-01-18 15:17:54 -0500

    Sinatra API
    - Added web API to retrieve PR activity
    - API access to: all open PRs, specific PRs, multiple specific PRs.

--- server/github-pr-monitor.rb
+++ server/github-pr-monitor.rb
@@ -10,4 +10,20 @@ pr_activity = GithubPR.new(Owner, Repo, Token)
 # puts pr_activity.fetch_all_open_pull_requests.to_json
 # puts pr_activity.fetch_pull_request(4710).to_json
 
-puts pr_activity.fetch_multiple_pull_requests([4709, 4710])
+# puts pr_activity.fetch_multiple_pull_requests([4709, 4710])
+
+require 'sinatra'
+
+get '/all' do
+  pr_activity.fetch_all_open_pull_requests.to_json
+end
+
+get '/pull/:number' do |num|
+	pr_activity.fetch_pull_request(num.to_i).to_json
+end
+
+get '/pulls/:numbers' do |nums|
+	numbers = nums.split(",")
+	numbers.map { |str| str.to_i }
+	pr_activity.fetch_multiple_pull_requests(numbers).to_json
+end
